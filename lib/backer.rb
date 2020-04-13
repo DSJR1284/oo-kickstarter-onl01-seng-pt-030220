64b6ff192ba7419bc8e3050f24bc8104316cf024
Doug Jones <dsjr1284@gmail.com>
2020-04-13 17:32:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,7 +3,7 @@ class Backer
   attr_reader :name, :backed_projects
 
 
-  def initialize(name, backed_projects)
+  def initialize(name,backed_projects)
     @name = name
     @backed_projects = []
   end
