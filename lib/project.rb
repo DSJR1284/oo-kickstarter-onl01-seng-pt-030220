371ba2f91d9c5f0a3c5b2fec980a4a6f1e3b25f7
Doug Jones <dsjr1284@gmail.com>
2020-04-13 17:38:38 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,13 @@
 class Project
 
-  attr_accessor :title, :backers 
+  attr_accessor :title, :backers
 
 
 
   def initialize(title)
-    @title = title 
+    @title = title
     @backers = []
   end
 
 
-end 
+end
